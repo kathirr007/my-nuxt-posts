bb51f01eb9cedde4db37134f75ef92943f302026
kkumarasamy <kkumarasamy@edit-place.com>
2019-11-21 19:53:16 +0530

    Postitem compnent created

---
+++ components/PostItem.vue
@@ -0,0 +1,43 @@
+<template>
+  <v-card>
+    <v-card-title class="headline">
+      Welcome to the Vuetify + Nuxt.js template
+    </v-card-title>
+    <v-card-subtitle class="subtitle-1 font-weight-normal white--text">
+      Here goes the subtitle
+    </v-card-subtitle>
+    <v-card-text>
+      <p>Blog contents goes here...</p>
+
+      <div class="text-xs-right">
+        <em><small>&mdash; John Leider</small></em>
+      </div>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer />
+      <v-btn dark nuxt to="/inspire">
+        View indetails
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+  export default {
+
+  }
+
+</script>
+
+<style lang="scss" scoped>
+  .posts-list {
+    .v-card {
+      margin-bottom: 15px;
+
+      :last-child {
+        margin: 0;
+      }
+    }
+  }
+
+</style>
