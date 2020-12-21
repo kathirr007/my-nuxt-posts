7bdaed562bba65a9226a55d47113ae017aef5d8a
kkumarasamy <kkumarasamy@edit-place.com>
2020-12-22 00:01:08 +0530

    Update icons

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,8 @@
 <template>
   <v-app dark>
-    <v-navigation-drawer
-      v-model="drawer"
-      :mini-variant="miniVariant"
-      :clipped="clipped"
-      fixed
-      app
-    >
+    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
       <v-list>
-        <v-list-item
-          v-for="(item, i) in menuItems"
-          :key="i"
-          :to="item.to"
-          router
-          exact
-        >
+        <v-list-item v-for="(item, i) in menuItems" :key="i" :to="item.to" router exact>
           <v-list-item-action>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-action>
@@ -24,11 +12,7 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar
-      :clipped-left="clipped"
-      fixed
-      app
-    >
+    <v-app-bar :clipped-left="clipped" fixed app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title>
         <nuxt-link to="/" tag="span" style="cursor: pointer">{{title}}</nuxt-link>
@@ -37,59 +21,63 @@
       <v-tooltip bottom>
         <template v-slot:activator="{ on }">
           <!-- <v-btn color="primary" dark v-on="on">Button</v-btn> -->
-          <v-btn icon dark v-on="on" class="mr-2" href="https://github.com/kathirr007/my-nuxt-posts" target="_blank">
-            <v-icon>mdi-github-circle</v-icon>
+          <v-btn
+            icon
+            dark
+            v-on="on"
+            class="mr-2"
+            href="https://github.com/kathirr007/my-nuxt-posts"
+            target="_blank"
+          >
+            <v-icon>mdi-github</v-icon>
           </v-btn>
         </template>
         <span>View on Github</span>
       </v-tooltip>
       <v-tooltip bottom>
         <template v-slot:activator="{ on }">
-          <v-btn icon dark class="mr-2" v-on="on" href="https://github.com/kathirr007/nuxt-posts" target="_blank">
-            <v-icon>mdi-github-circle</v-icon>
+          <v-btn
+            icon
+            dark
+            class="mr-2"
+            v-on="on"
+            href="https://github.com/kathirr007/nuxt-posts"
+            target="_blank"
+          >
+            <v-icon>mdi-github</v-icon>
           </v-btn>
         </template>
         <span>View on Github of original reference</span>
       </v-tooltip>
 
       <!-- <a href="https://github.com/kathirr007/nuxt-posts" target="_blank">
-      </a> -->
+      </a>-->
       <v-menu
-      bottom
-      origin="center center"
-      transition="slide-y-transition"
-      class="d-none d-sm-block"
-    >
-      <template v-slot:activator="{ on }">
-        <v-btn
-          dark
-          v-on="on"
-          class="d-none d-sm-block"
-        >
-          Menu
-        </v-btn>
-      </template>
+        bottom
+        origin="center center"
+        transition="slide-y-transition"
+        class="d-none d-sm-block"
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn dark v-on="on" class="d-none d-sm-block">Menu</v-btn>
+        </template>
 
-      <v-list>
-        <v-list-item
-          v-for="(item, i) in menuItems"
-          :key="i"
-          :to="item.to"
-        >
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-          <!-- <v-list-item-title>{{ item.title }}</v-list-item-title> -->
-        </v-list-item>
-      </v-list>
-    </v-menu>
+        <v-list>
+          <v-list-item v-for="(item, i) in menuItems" :key="i" :to="item.to">
+            <v-list-item-action>
+              <v-icon>{{ item.icon }}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title v-text="item.title" />
+            </v-list-item-content>
+            <!-- <v-list-item-title>{{ item.title }}</v-list-item-title> -->
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-app-bar>
     <v-content>
       <v-container fluid>
-          <nuxt />
+        <nuxt />
       </v-container>
     </v-content>
   </v-app>
@@ -97,29 +85,30 @@
 
 <script>
 export default {
-  data () {
+  data() {
     return {
       clipped: false,
       drawer: false,
       fixed: false,
       menuItems: [
         {
-          icon: 'mdi-home',
-          title: 'Home',
-          to: '/'
-        }, {
-          icon: 'mdi-chart-bubble',
-          title: 'Manage',
-          to: '/manage'
-        }
+          icon: "mdi-home",
+          title: "Home",
+          to: "/",
+        },
+        {
+          icon: "mdi-chart-bubble",
+          title: "Manage",
+          to: "/manage",
+        },
       ],
       miniVariant: false,
       right: true,
       rightDrawer: false,
-      title: 'Blog App using Nuxt.js And Vuetify'
-    }
-  }
-}
+      title: "Blog App using Nuxt.js And Vuetify",
+    };
+  },
+};
 </script>
 
 <style lang="scss">
--- pages/manage.vue
+++ pages/manage.vue
@@ -5,19 +5,29 @@
         <post-create />
       </v-col>
 
-      <simplebar class="posts-list py-md-0  float-sm-left order-2 order-md-1" :class="[activePost ? 'col-md-4' : 'col-md-10']" v-if="posts && posts.length > 0">
+      <simplebar
+        class="posts-list py-md-0 float-sm-left order-2 order-md-1"
+        :class="[activePost ? 'col-md-4' : 'col-md-10']"
+        v-if="posts && posts.length > 0"
+      >
         <v-col cols="12" class="py-0" transition="scale-transition">
           <v-slide-y-transition group>
             <v-hover v-for="post in posts" :key="post._id" v-slot:default="{ hover }">
-              <v-card :class="{'is-active': activePost && post._id === activePost._id}" color="" dark :elevation="hover ? 12 : 2" @click.native="activatePost(post)">
+              <v-card
+                :class="{'is-active': activePost && post._id === activePost._id}"
+                color
+                dark
+                :elevation="hover ? 12 : 2"
+                @click.native="activatePost(post)"
+              >
                 <v-card-title class="headline">{{post.title}}</v-card-title>
-                <v-card-subtitle class="font-italic">
-                  {{post.subtitle}}
-                </v-card-subtitle>
+                <v-card-subtitle class="font-italic">{{post.subtitle}}</v-card-subtitle>
                 <v-card-text>
                   <p>{{post.content}}</p>
                   <div class="text-right">
-                    <em><small>From John Leider</small></em>
+                    <em>
+                      <small>From John Leider</small>
+                    </em>
                   </div>
                 </v-card-text>
               </v-card>
@@ -25,9 +35,16 @@
           </v-slide-y-transition>
         </v-col>
       </simplebar>
-      <simplebar v-if="activePost && posts.length > 0" cols="12" md="8" class="col col-12 col-md-8 py-md-0 order-1 order-md-2 post-manage pa-5" id="message-preview" transition="scale-transition">
+      <simplebar
+        v-if="activePost && posts.length > 0"
+        cols="12"
+        md="8"
+        class="col col-12 col-md-8 py-md-0 order-1 order-md-2 post-manage pa-5"
+        id="message-preview"
+        transition="scale-transition"
+      >
         <transition name="fade" mode="out-in">
-          <post-manage @deletePostSubmitted='deletePost' :postData="activePost" />
+          <post-manage @deletePostSubmitted="deletePost" :postData="activePost" />
         </transition>
       </simplebar>
       <simplebar v-else cols="10" transition="scale-transition" class="post-manage col col-10">
@@ -36,112 +53,125 @@
         </v-card>
       </simplebar>
       <!-- <simplebar class="post-manage">
-      </simplebar> -->
-
+      </simplebar>-->
     </v-row>
     <footer class="footer">
       <v-divider />
       <v-container fluid class="text-center py-0">
-          <v-col cols="12">
-            <strong>Blog Created </strong> by <a href="https://github.com/kathirr007">kathirr007</a>. The source
-            code is available at
-            <v-tooltip top tag="span">
-              <template v-slot:activator="{ on }">
-                <v-btn icon dark v-on="on" class="mr-2" href="https://github.com/kathirr007/my-nuxt-posts" target="_blank">
-                  <v-icon>mdi-github-circle</v-icon>
-                </v-btn>
-              </template>
-              <span>View on Github</span>
-            </v-tooltip>
-          </v-col>
+        <v-col cols="12">
+          <strong>Blog Created</strong> by
+          <a href="https://github.com/kathirr007">kathirr007</a>. The source
+          code is available at
+          <v-tooltip top tag="span">
+            <template v-slot:activator="{ on }">
+              <v-btn
+                icon
+                dark
+                v-on="on"
+                class="mr-2"
+                href="https://github.com/kathirr007/my-nuxt-posts"
+                target="_blank"
+              >
+                <v-icon>mdi-github</v-icon>
+              </v-btn>
+            </template>
+            <span>View on Github</span>
+          </v-tooltip>
+        </v-col>
       </v-container>
     </footer>
   </div>
 </template>
 
 <script>
-  import { mapState } from 'vuex'
-  import PostCreate from '~/components/PostCreate'
-  import PostManage from '~/components/PostManage'
-  import simplebar from 'simplebar-vue';
-  import 'simplebar/dist/simplebar.min.css';
-  import goTo from 'vuetify/es5/services/goto'
-  import axios from 'axios'
-  export default {
-    components: {
-      PostCreate, PostManage, simplebar
-    },
-    data() {
-      return {
-        // posts: this.$store.state.posts,
-        activePost: null,
+import { mapState } from "vuex";
+import PostCreate from "~/components/PostCreate";
+import PostManage from "~/components/PostManage";
+import simplebar from "simplebar-vue";
+import "simplebar/dist/simplebar.min.css";
+import goTo from "vuetify/es5/services/goto";
+import axios from "axios";
+export default {
+  components: {
+    PostCreate,
+    PostManage,
+    simplebar,
+  },
+  data() {
+    return {
+      // posts: this.$store.state.posts,
+      activePost: null,
+    };
+  },
+  computed: {
+    ...mapState({
+      posts: (state) => state.posts.items,
+    }),
+    isActivePost() {
+      if (this.activePost && this.post._id === this.activePost._id) {
+        return "primary";
       }
     },
-    computed: {
-      ...mapState({
-        posts: (state) => state.posts.items
-      }),
-      isActivePost() {
-        if(this.activePost && this.post._id === this.activePost._id) {
-          return 'primary'
-        }
-      }
-    },
-    fetch({store}) {
-      if(store.state.posts.items.length === 0) {
-        console.log('Fetching data in manage page')
-        return store.dispatch('posts/fetchPosts')
-      }
-    },
-    created() {
-      this.setInitialActivePost()
-    },
-    methods: {
-      /* async testAxios () {
+  },
+  fetch({ store }) {
+    if (store.state.posts.items.length === 0) {
+      console.log("Fetching data in manage page");
+      return store.dispatch("posts/fetchPosts");
+    }
+  },
+  created() {
+    this.setInitialActivePost();
+  },
+  methods: {
+    /* async testAxios () {
         const data = await axios.get('/missions', {})
         .then((response) =>{
           console.log(response)
         })
       }, */
-      activatePost(post) {
-        this.activePost = post
-        goTo('.v-content', {
-          duration: 300,
-          offset: 0,
-          easing: 'easeInOutCubic'
-        })
-      },
-      setInitialActivePost() {
-        if (this.posts && this.posts.length > 0) {
-          this.activePost = this.posts[0]
-        } else {
-          this.activePost = null
-        }
-      },
-      deletePost(){
-        if(this.activePost) {
-          const index = this.posts.findIndex((post) => {
-            return post._id === this.activePost._id
-          })
-          this.$store.dispatch('posts/deletePost', this.activePost._id)
-            .then(() => {
-              // this.setInitialActivePost()
-              this.activePost = this.posts[index - 1]
-            })
-        }
+    activatePost(post) {
+      this.activePost = post;
+      goTo(".v-content", {
+        duration: 300,
+        offset: 0,
+        easing: "easeInOutCubic",
+      });
+    },
+    setInitialActivePost() {
+      if (this.posts && this.posts.length > 0) {
+        this.activePost = this.posts[0];
+      } else {
+        this.activePost = null;
       }
     },
-    transition (to, from) {
-      if (!from) { return 'slide-left' }
-      return  'slide-right'
+    deletePost() {
+      if (this.activePost) {
+        const index = this.posts.findIndex((post) => {
+          return post._id === this.activePost._id;
+        });
+        this.$store
+          .dispatch("posts/deletePost", this.activePost._id)
+          .then(() => {
+            // this.setInitialActivePost()
+            this.activePost = this.posts[index - 1];
+          });
+      }
     },
-  }
+  },
+  transition(to, from) {
+    if (!from) {
+      return "slide-left";
+    }
+    return "slide-right";
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.posts-list, .post-manage {
+.posts-list,
+.post-manage {
   // height: calc(100vh - (56px + 60px + 100px + 24px));
-  @media screen and(min-width: 960px){
+  @media screen and(min-width: 960px) {
     overflow: hidden;
     overflow-y: auto;
     height: calc(100vh - (64px + 60px + 76px + 24px));
@@ -149,17 +179,18 @@
 }
 .v-card {
   margin-bottom: 15px;
- &:last-child, &:only-child {
-   margin-bottom: 0;
- }
- &:hover {
-   cursor: pointer;
- }
- &.no-hover {
-   &:hover {
-     cursor: default;
-   }
- }
+  &:last-child,
+  &:only-child {
+    margin-bottom: 0;
+  }
+  &:hover {
+    cursor: pointer;
+  }
+  &.no-hover {
+    &:hover {
+      cursor: default;
+    }
+  }
 }
 .headline {
   word-break: break-word;
